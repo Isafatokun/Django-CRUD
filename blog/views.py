bd8fac8639c86d52500a1563bd4efe2b098d5f18
BiG Zaddy Ziki <isaac.fatokun@yahoo.com>
2022-06-25 18:43:27 +0200

    Code

---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy('blog:all')
+    template_name = 'post_form.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy('blog:all')
+    template_name = 'post_form.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy('blog:all')
+    template_name = 'post_confirm_delete.html'
+
